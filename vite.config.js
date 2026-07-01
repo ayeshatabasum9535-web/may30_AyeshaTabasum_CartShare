aa8e092057d99aba0ca3a1aaea3a2761166454c2
ayesha tabasum j <ayeshatabasum9535@gmail.com>
2026-07-01 18:16:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        // Enforce the strict course folder layout structure inside the compiled /dist folder
+        
         entryFileNames: 'js/[name]-[hash].js',
         chunkFileNames: 'js/[name]-[hash].js',
         assetFileNames: ({ name }) => {
